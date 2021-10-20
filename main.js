5df578da451c3f7b9a377d30713c245bdc32485a
NicolaL95 <nicola.lilla@gmail.com>
2021-10-20 14:46:58 +0200

    Fine lavorazione

---
+++ main.js
@@ -0,0 +1,30 @@
+const mode = prompt("inserisci la difficoltà a cui vuoi giocare: 1-facile(100 caselle) 2-media(81 caselle) 3-difficile(49 caselle)")
+let selMode = 0;
+const containerGrid = document.querySelector(".container");
+if (mode == 1) {
+    selMode = 100;
+}
+
+else if (mode == 2) {
+    selMode = 81;
+}
+
+else if (mode == 3) {
+    selMode = 49;
+}
+
+else {
+    window.alert("non hai inserito una difficoltà valida!")
+}
+
+for (let index = 1; index <= selMode; index++) {
+    const divGrid = document.createElement("div")
+    divGrid.className = "cella"
+
+    containerGrid.append(divGrid);
+    divGrid.innerHTML = index;
+
+    divGrid.addEventListener("click", function () {
+        this.style.backgroundColor = "cyan"
+    })
+}
